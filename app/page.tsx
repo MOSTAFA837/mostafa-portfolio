a5067eb26353ffdca3351e4ad012ffd78dfd83fc
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-06-30 21:45:42 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>mostafa&apos;s portfolio</h1>
+      <UserButton />
+    </main>
+  );
+}
